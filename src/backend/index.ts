4e6e7ad4103c7864dd74c2896f84566e30d367cc
Bennett Hardwick <bennetthardwick@icloud.com>
2018-04-24 17:23:08 +1000

    fixed some stuff

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -9,7 +9,7 @@ let win: BrowserWindow;
 function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
   win.loadURL(url.format({
-    pathname: path.join(__dirname, '..', 'public', 'index.html'),
+    pathname: path.join(__dirname, '..', '..', 'public', 'index.html'),
     protocol: 'file:', 
     slashes: true
   }));
